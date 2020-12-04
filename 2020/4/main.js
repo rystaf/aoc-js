0c3b663f4d2519ff5316f8e34cef19bdb6956fb6
Ryan Stafford <staffordrj@gmail.com>
2020-12-04 06:21:04 -0500

    day 4 part 2

--- 2020/4/main.js
+++ 2020/4/main.js
@@ -1,7 +1,11 @@
 main = input => {
-  input = input.split('\n\n').map(x => x.replace(/\n/g,' '))
-  var part1 = input
-    .map(x => x.split(' ').map(f => f.split(':')[0]))
+  input = input.split('\n\n').map(p => p.replace(/\n/g,' '))
+    .map(p => p.split(' ').reduce((a,b) => ({
+      ...a,
+      [b.split(':')[0]]: b.split(':')[1]
+    }), {}))
+
+  var valid = input
     .filter(x => [
       'byr',
       'iyr',
@@ -10,7 +14,28 @@ main = input => {
       'hcl',
       'ecl',
       'pid',
-    ].every(f => x.includes(f)))
+    ].every(f => Object.keys(x).includes(f)))
+
+  var part1 = valid.length
+
+  String.prototype.btwn = function(min,max) { 
+    return this >= min && this <= max
+  }
+  
+  var part2 = valid
+    .filter(x => [
+      ({byr}) => byr.btwn(1920, 2002),
+      ({iyr}) => iyr.btwn(2010, 2020),
+      ({eyr}) => eyr.btwn(2020, 2030),
+      ({hgt}) => /^\d+(cm|in)$/.test(hgt),
+      ({hgt}) => hgt.includes('cm')
+        ? hgt.replace(/\D/g, "").btwn(150, 193)
+        : hgt.replace(/\D/g, "").btwn(59, 76),
+      ({hcl}) => /^#[\da-f]{6}$/.test(hcl),
+      ({ecl}) => ['amb','blu','brn','gry','grn','hzl','oth'].some(c => ecl == c),
+      ({pid}) => /^\d{9}$/.test(pid),
+    ].every(f => f(x)))
     .length
-  return [part1]
+
+  return [part1, part2]
 }

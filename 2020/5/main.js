92d4c3a68fe3d1c3db00a52affbaa8e25c5a7d2c
Ryan Stafford <staffordrj@gmail.com>
2020-12-05 11:19:11 -0500

    2020 day 5 part 2

--- 2020/5/main.js
+++ 2020/5/main.js
@@ -4,6 +4,12 @@ main = input => {
       .map(x => ['F','L'].includes(x) ? '0' : '1'))
     .map(x => [x.slice(0,7), x.slice(7)].map(n => parseInt(n.join(''), 2)))
     .map(([row, column]) => row * 8 + column)
+
   var part1 = Math.max(...passes)
-  return [part1]
+
+  var part2 = passes
+    .map((x,i,s) => x == part1 || s.includes(x+1) ? 0 : x + 1)
+    .find(x => x)
+
+  return [part1, part2]
 }

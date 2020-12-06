0dead5fe30350662b66ea5d3cc7413834a780be7
Ryan Stafford <staffordrj@gmail.com>
2020-12-06 00:28:05 -0500

    day 6 part 2

--- 2020/6/main.js
+++ 2020/6/main.js
@@ -1,9 +1,21 @@
 main = input => {
+
   var groups = input.split('\n\n')
+
+  var part1 = groups
     .map(g => g.replace(/\n/g,'').split('')
       .reduce((a, b) => a.includes(b) ? a : a + b, ''))
     .map(x => x.length)
     .reduce((a, b) => a+b,0)
-  return [groups]
+ 
+  var part2 = groups
+    .map(g => g.replace(/\n/g,'').split('')
+      .reduce((a, b) => a.includes(b) ? a : a + b, '')
+      .split('')
+      .filter(q => g.split('\n').every(x => x.includes(q)))
+      .length
+    ).reduce((a, b) => a+b, 0)
+
+  return [part1, part2]
 
 }

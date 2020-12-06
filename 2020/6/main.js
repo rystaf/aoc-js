e262324596405199ec5ab294a6e19422c6eda449
Ryan Stafford <staffordrj@gmail.com>
2020-12-06 00:42:30 -0500

    touch up

--- 2020/6/main.js
+++ 2020/6/main.js
@@ -3,18 +3,17 @@ main = input => {
   var groups = input.split('\n\n')
 
   var part1 = groups
-    .map(g => g.replace(/\n/g,'').split('')
-      .reduce((a, b) => a.includes(b) ? a : a + b, ''))
-    .map(x => x.length)
-    .reduce((a, b) => a+b,0)
+    .map(g => g.replace(/\n/g, '').split('')
+      .reduce((a, b) => a.includes(b) ? a : a + b, '')
+      .length)
+    .reduce((a, b) => a + b, 0)
  
   var part2 = groups
-    .map(g => g.replace(/\n/g,'').split('')
-      .reduce((a, b) => a.includes(b) ? a : a + b, '')
-      .split('')
+    .map(g => g.replace(/\n/g, '').split('')
+      .reduce((a, b) => a.includes(b) ? a : [...a, b], '')
       .filter(q => g.split('\n').every(x => x.includes(q)))
-      .length
-    ).reduce((a, b) => a+b, 0)
+      .length)
+    .reduce((a, b) => a + b, 0)
 
   return [part1, part2]
 

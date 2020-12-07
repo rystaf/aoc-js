100cbdb7f52791c7f886eedc1d2dbd58a0cdd318
Ryan Stafford <staffordrj@gmail.com>
2020-12-07 15:52:58 -0500

    caniuse flatMap? yesican

--- 2020/7/main.js
+++ 2020/7/main.js
@@ -3,26 +3,23 @@ main = input => {
   const rules = input.split('\n').map(b => ({
     container: b.split(' bags contain ')[0],
     contains: [...b.matchAll(/(\d+) ([a-z]+ [a-z]+) bags?/g)]
-      .map(x => [...Array(Number(x[1]))].map(n => x[2]))
-      .reduce((a, b) => Array.isArray(b) ? [...a, ...b] : [...a, b], [])
+      .flatMap(x => [...Array(Number(x[1]))].map(n => x[2]))
   }))
 
   const canContain = bag => rules
     .filter(x => x.contains.includes(bag))
-    .map(x => [x.container, ...canContain(x.container)])
-    .reduce((a, b) => Array.isArray(b) ? [...a, ...b] : [...a, b], [])
-    .filter((x,i,s) => !s.slice(i+1).includes(x))
+    .flatMap(x => [x.container, ...canContain(x.container)])
+    .filter((x, i, s) => !s.slice(i + 1).includes(x))
 
   const part1 = canContain('shiny gold').length
 
   const mustContain = bag => rules
     .filter(x => x.container == bag)
-    .map(x => x.contains)
-    .reduce((a, b) => Array.isArray(b) ? [...a, ...b] : [...a, b], [])
-    .map(x => [x, ...mustContain(x)])
-    .reduce((a, b) => Array.isArray(b) ? [...a, ...b] : [...a, b], [])
+    .flatMap(x => x.contains)
+    .flatMap(x => [x, ...mustContain(x)])
 
   const part2 = mustContain('shiny gold').length
 
   return [part1, part2]
+
 }

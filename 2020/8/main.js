b4e98c27e0d4ee0952bd0312990f7a92fed723c7
Ryan Stafford <staffordrj@gmail.com>
2020-12-08 13:09:23 -0500

    day 8 part 2

--- 2020/8/main.js
+++ 2020/8/main.js
@@ -1,19 +1,29 @@
 main = input => {
-
+  Array.prototype.copy = function() {
+    return JSON.parse(JSON.stringify(this))
+  }
   const instructions = input.split('\n')
     .map(x => x.split(' ').map((y,i) => i?Number(y):y))
 
-  const compute = (i=0, acc=0) => {
-    const [op, arg] = instructions[i]
-    if (!op) return acc
-    instructions[i][0] = false
-    if (op == 'nop') return compute(i + 1, acc)
-    if (op == 'acc') return compute(i + 1, acc + arg)
-    if (op == 'jmp') return compute(i + arg, acc)
+  const compute = (inst, i=0, acc=0) => {
+    if (i == inst.length) return [acc, 1]
+    const [op, arg] = inst[i]
+    if (!op) return [acc, 0]
+    inst[i][0] = false
+    if (op == 'nop') return compute(inst, i + 1, acc)
+    if (op == 'acc') return compute(inst, i + 1, acc + arg)
+    if (op == 'jmp') return compute(inst, i + arg, acc)
   }
 
-  const part1 = compute()
+  const part1 = compute(instructions.copy())[0]
 
-  return [part1]
+  const part2 = instructions.map((x,i,s) => {
+    const inst = s.copy()
+    if (x[0] == 'nop') inst[i][0] = 'jmp'
+    if (x[0] == 'jmp') inst[i][0] = 'nop'
+    return compute(inst)
+  }).find(x => x[1])[0]
+
+  return [part1, part2]
 
 }

92e4f4aa6da45bfd897784a0720dc4a5b31ecd32
Ryan Stafford <staffordrj@gmail.com>
2020-12-08 20:19:28 -0500

    removed need for deep copy

--- 2020/8/main.js
+++ 2020/8/main.js
@@ -1,28 +1,26 @@
 main = input => {
-  Array.prototype.copy = function() {
-    return JSON.parse(JSON.stringify(this))
-  }
+
   const instructions = input.split('\n')
-    .map(x => x.split(' ').map((y,i) => i?Number(y):y))
 
   const compute = (inst, i=0, acc=0) => {
     if (i == inst.length) return [acc, 1]
-    const [op, arg] = inst[i]
-    if (!op) return [acc, 0]
-    inst[i][0] = false
-    if (op == 'nop') return compute(inst, i + 1, acc)
-    if (op == 'acc') return compute(inst, i + 1, acc + arg)
-    if (op == 'jmp') return compute(inst, i + arg, acc)
+    if (!inst[i]) return [acc, 0]
+    const [op, arg] = inst[i].split(' ').map((x, i) => i ? Number(x) : x)
+    inst[i] = false
+    if (op == 'nop') i += 1
+    if (op == 'acc') { i += 1; acc += arg }
+    if (op == 'jmp') i += arg
+    return compute(inst, i, acc)
   }
 
-  const part1 = compute(instructions.copy())[0]
+  const [part1] = compute([...instructions])
 
-  const part2 = instructions.map((x,i,s) => {
-    const inst = s.copy()
-    if (x[0] == 'nop') inst[i][0] = 'jmp'
-    if (x[0] == 'jmp') inst[i][0] = 'nop'
+  const [part2] = instructions.map((x, i, s) => {
+    if (!/(jmp|nop)/.test(x)) return [0, 0]
+    const inst = [...s]
+    inst[i] = s[i].replace(/^\S+/, /nop/.test(s[i]) ? 'jmp' : 'nop')
     return compute(inst)
-  }).find(x => x[1])[0]
+  }).find(x => x[1])
 
   return [part1, part2]
 

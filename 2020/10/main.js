94aa1adb2fec44f1df1086a7e5e2eecea63854ce
Ryan Stafford <staffordrj@gmail.com>
2020-12-11 08:37:59 -0500

    day 10 part 2: sample tests work, final input never finishes. need something more efficient

--- 2020/10/main.js
+++ 2020/10/main.js
@@ -1,11 +1,24 @@
 main = input => {
+
   const adapters = input.split('\n').map(x => Number(x)).sort((a,b)=>a-b)
+
   const part1 = [...adapters, adapters[adapters.length-1]+3]
-    .reduce(([j,d1,d3],a) => [
+    .reduce(([j, d1, d3], a) => [
       a,
       (a-j) == 1 ? d1 += 1 : d1,
       (a-j) == 3 ? d3 += 1 : d3
-    ], [0,0,0])
+    ], [0, 0, 0])
     .reduce((a, b, i) => i ? a*b : a, 1)
-  return [part1]
+
+  const findAdapters = (a) => {
+    const found = a.filter(x => (x - a[0]) > 0 && (x - a[0]) < 4 ) 
+    if (found.length) return found.map(f => findAdapters(a.slice(a.indexOf(f))))
+      .reduce((x, y) => x + y,0)
+    else return 1
+  }  
+
+  const part2 = findAdapters([0, ...adapters, adapters[adapters.length-1]+3])
+
+  return [part1, part2]
+
 }
--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
-const [ year, day ] = process.argv.slice(2)
+const [ year, day, file] = process.argv.slice(2)
 const dir = `./${year}/${day}`
 require(`${dir}/main.js`)
 fs.readdirSync(dir)
-  .filter(x => x.includes('.txt'))
+  .filter(x => x.includes((file || '')+'.txt'))
   .map(x => {
     let input = fs.readFileSync(`${dir}/${x}`, 'utf8')
     let outputFile = `${dir}/${x.split('.')[0]}.json`;

d9305f8dc2505f1e80aff74cc66ed4063bd6960b
Ryan Stafford <staffordrj@gmail.com>
2020-12-11 16:02:21 -0500

    day 10 part 2 with the help of coworker. still need to calculate orientations

--- 2020/10/main.js
+++ 2020/10/main.js
@@ -10,12 +10,15 @@ main = input => {
     ], [0, 0, 0])
     .reduce((a, b, i) => i ? a*b : a, 1)
 
-  const findAdapters = (a) => a
-    .filter(x => (x - a[0]) > 0 && (x - a[0]) < 4 ) 
-    .map(f => findAdapters(a.slice(a.indexOf(f))))
-    .reduce((x, y, i) => x + y - !i, 1)
-
-  const part2 = findAdapters([0, ...adapters, adapters[adapters.length-1]+3])
+  const [part2] = [0, ...adapters, adapters[adapters.length-1]+3]
+    .reduce(([t,c], b, i, a) => {
+      if(b-a[i-1] == 1) return [t, c += 1]
+      else if (c == 2) {t *= 2; c=0}
+      else if (c == 3) {t *= 4; c=0}
+      else if (c == 4) {t *= 7; c=0}
+      else c=0
+      return [t,c]
+    },[1,0])
 
   return [part1, part2]
 

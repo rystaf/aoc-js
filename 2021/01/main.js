f49c7217c2e2ced71cc6d234d9ef70339d66acc1
Ryan Stafford <staffordrj@gmail.com>
2021-12-01 09:32:02 -0500

    2021 day 1 part 2

--- 2021/01/main.js
+++ 2021/01/main.js
@@ -3,7 +3,18 @@ main = input => {
   
   let numberOfDepthIncreases = 0
   for (let i = 0; i < depths.length; i += 1) {
-    if (i && depths[i] > depths [i-1]) numberOfDepthIncreases += 1
+    if (i && depths[i] > depths [i-1]) {
+      numberOfDepthIncreases += 1
+    }
   }
-  return [numberOfDepthIncreases, 0]
+
+  let numberOfDepthIncreasesIn3MeasurementWindow = 0
+  for (let i = 0; i < depths.length; i += 1) {
+    const window1 = depths.slice(i, i+3).reduce((a,b) => a+b,0)
+    const window2 = depths.slice(i+1, i+4).reduce((a,b) => a+b,0)
+    if (window2 > window1) {
+      numberOfDepthIncreasesIn3MeasurementWindow += 1
+    }
+  }
+  return [numberOfDepthIncreases, numberOfDepthIncreasesIn3MeasurementWindow]
 }

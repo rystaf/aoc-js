2b34afcbe407b6c40d16ffac4cf4aea98dcc9f6e
Ryan Stafford <staffordrj@gmail.com>
2021-12-02 09:27:34 -0500

    2021 day 2 part 2

--- 2021/02/main.js
+++ 2021/02/main.js
@@ -14,5 +14,20 @@ main = input => {
     .map(x => x[0] == 'up' ? x[1] * -1 : x[1])
     .reduce((a, b) => a + b, 0)
 
-  return [horizontalSum * verticalSum, 0]
+  const part1 = horizontalSum * verticalSum
+
+  let aim = 0
+  const position = [0, 0]
+  for (let i = 0; i < steps.length; i += 1) {
+    const [direction, distance] = steps[i]
+    if (direction == 'forward') {
+      position[0] += distance
+      position[1] += (distance * aim)
+    }
+    if (direction == 'up') aim -= distance
+    if (direction == 'down') aim += distance
+  }
+  const part2 = position.reduce((a,b) => a*b,1)
+
+  return [part1, part2]
 }

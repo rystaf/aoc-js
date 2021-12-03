fa306b92131a147e367561a057035e75e0256aab
Ryan Stafford <ryan@nanorex.net>
2021-12-03 10:07:02 -0500

    2021 day 3 part 2

--- 2021/03/main.js
+++ 2021/03/main.js
@@ -1,8 +1,8 @@
 main = input => {
-  const numbers = input.split('\n')
+  const numbers = input.split('\n').map(x => x.trim())
   let gammaRate = ''
   let epsilonRate = ''
-
+  
   // loop over the length of the first number. Assuming all numbers are the same length.
   for (const i in numbers[0]) {
     const positionNumbers = numbers.map(x => x[i])
@@ -15,5 +15,18 @@ main = input => {
 
   const part1 = parseInt(gammaRate, 2) * parseInt(epsilonRate, 2)
 
-  return [part1,0]
+  const findRating = (bit, options, i=0) => {
+    if (options.length == 1) return parseInt(options[0], 2)
+    const group = [
+      options.filter(x => x[i] === '0'),
+      options.filter(x => x[i] === '1')
+    ]
+    if (group[1].length == group[0].length) return findRating(bit, group[bit], i += 1)
+    const most = (group[0].length > group[1].length ? 0 : 1)
+    return findRating(bit, group[bit ? most : 1 - most], i += 1)
+  }
+  
+  const part2 = findRating(0, numbers) * findRating(1, numbers)
+
+  return [part1, part2]
 }

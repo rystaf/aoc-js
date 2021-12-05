a56202536723e75b5a2dc76862a4032de3734964
Ryan Stafford <ryan@nanorex.net>
2021-12-05 17:35:35 -0500

    added sample answers

--- 2021/05/main.js
+++ 2021/05/main.js
@@ -6,14 +6,14 @@ main = input => {
   const xMax = lines.flatMap(x => x[0]).sort((a,b) => b-a)[0]
   const yMax = lines.flatMap(x => x[1]).sort((a,b) => b-a)[0]
   
-  const findOverlaps = (includeDiagonal = false) => {
+  const findOverlaps = (ignoreDiagonal = true) => {
     // create grid of zeroes
     const grid = Array.apply(null, Array(yMax+1))
       .map(()=>Array.apply(null, Array(xMax+1)).map(x => 0))
 
     for (const line of lines) {
       const [[x1,y1],[x2,y2]] = line
-      if (includeDiagonal && x1 != x2 && y1 != y2) continue
+      if (ignoreDiagonal && x1 != x2 && y1 != y2) continue
       let [xi, yi] = [x1, y1]
       grid[y2][x2] += 1
       while (xi != x2 || yi != y2) {
@@ -26,5 +26,5 @@ main = input => {
     }
     return grid.flatMap(x => x).filter(x => x > 1).length
   }
-  return [findOverlaps(), findOverlaps(true)]
+  return [findOverlaps(), findOverlaps(false)]
 }

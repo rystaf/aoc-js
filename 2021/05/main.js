9c68423780a9a1efffd4efadf2e5d9362be801e8
Ryan Stafford <ryan@nanorex.net>
2021-12-05 15:07:52 -0500

    2021 day 5 part 2

--- 2021/05/main.js
+++ 2021/05/main.js
@@ -6,23 +6,25 @@ main = input => {
   const xMax = lines.flatMap(x => x[0]).sort((a,b) => b-a)[0]
   const yMax = lines.flatMap(x => x[1]).sort((a,b) => b-a)[0]
   
-  // create grid of zeroes
-  const floor = Array.apply(null, Array(yMax+1))
-    .map(()=>Array.apply(null, Array(xMax+1)).map(x => 0))
+  const findOverlaps = (includeDiagonal = false) => {
+    // create grid of zeroes
+    const grid = Array.apply(null, Array(yMax+1))
+      .map(()=>Array.apply(null, Array(xMax+1)).map(x => 0))
 
-  for (const line of lines) {
-    const [[x1,y1],[x2,y2]] = line
-    if (x1 != x2 && y1 != y2) continue
-    let [xi, yi] = [x1, y1]
-    floor[y2][x2] += 1
-    while (xi != x2 || yi != y2) {
-      floor[yi][xi] += 1
-      if (x2 > xi) xi += 1
-      if (x2 < xi) xi -= 1
-      if (y2 > y1) yi += 1
-      if (y2 < y1) yi -= 1
+    for (const line of lines) {
+      const [[x1,y1],[x2,y2]] = line
+      if (includeDiagonal && x1 != x2 && y1 != y2) continue
+      let [xi, yi] = [x1, y1]
+      grid[y2][x2] += 1
+      while (xi != x2 || yi != y2) {
+        grid[yi][xi] += 1
+        if (x2 > xi) xi += 1
+        if (x2 < xi) xi -= 1
+        if (y2 > y1) yi += 1
+        if (y2 < y1) yi -= 1
+      }
     }
+    return grid.flatMap(x => x).filter(x => x > 1).length
   }
-  const part1 = floor.flatMap(x => x).filter(x => x > 1).length
-  return [part1, 0]
+  return [findOverlaps(), findOverlaps(true)]
 }

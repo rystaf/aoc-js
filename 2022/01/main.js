b42c9123feeacb2f0a5f5bff96b68ed6ffc89385
Ryan Stafford <ryan@nanorex.net>
2022-12-01 09:34:37 -0500

    part 2

--- 2022/01/main.js
+++ 2022/01/main.js
@@ -1,7 +1,7 @@
 main = input => { 
   var inventories = input.split('\n\n').map(elf => elf.split('\n').map(x => parseInt(x)))
-
-  var part1 = inventories.map(elf => elf.reduce((a,b)=> a+b,0)).sort((a,b)=>b-a)[0]
-
-  return [part1,0]
+  var sortedTotals = inventories.map(elf => elf.reduce((a,b)=> a+b,0)).sort((a,b)=>b-a)
+  var part1 = sortedTotals[0]
+  var part2 = sortedTotals.slice(0,3).reduce((a,b)=>a+b,0)
+  return [part1,part2]
 }

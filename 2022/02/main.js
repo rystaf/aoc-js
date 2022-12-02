d59cb3a6cbb3c3deb4571aabc3b726d773b59d40
Ryan Stafford <ryan@nanorex.net>
2022-12-02 01:21:39 -0500

    less maps

--- 2022/02/main.js
+++ 2022/02/main.js
@@ -1,14 +1,6 @@
 main = input => { 
   var games = input.split('\n').map(x => x.split(' '))
-  var code = {
-    A: 'r',
-    X: 'r',
-    B: 'p',
-    Y: 'p',
-    C: 's',
-    Z: 's'
-  }
-  var rank = 'rpsrp'
+  var rank = 'ABCAB'
   var score = (p1,p2) => {
     i = rank.indexOf(p1)
     i2 = rank.indexOf(p2) + 1
@@ -18,23 +10,17 @@ main = input => {
   }
 
   var part1 = games
-    .map(g => g.map(h=>code[h]))
+    .map(g => [g[0], String.fromCharCode(g[1].charCodeAt()-23)])
     .reduce((a,b)=>score(b[0],b[1])+a,0)
 
-  var code2 = {
-    A: 'r',
-    X: 2,
-    B: 'p',
-    Y: 0,
-    C: 's',
-    Z: 1
-  }
   var determineHand = (p1,p2) => {
     i = rank.indexOf(p1)
     return rank[i+p2]
   }
+
   var part2 = games
-    .map(g => g.map(h=>code2[h]))
+    .map(g => [g[0], ["Y","Z","X"].indexOf(g[1])])
     .reduce((a,b)=> score(b[0],determineHand(b[0], b[1]))+a,0)
+
   return [part1,part2]
 }

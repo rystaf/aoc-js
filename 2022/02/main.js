9707d74fbc9663f4570459d7c3e123eb585c5cae
Ryan Stafford <ryan@nanorex.net>
2022-12-02 00:29:20 -0500

    part 2

--- 2022/02/main.js
+++ 2022/02/main.js
@@ -7,7 +7,7 @@ main = input => {
     C: 's',
     Z: 's'
   }
-  var games = input.split('\n').map(x => x.split(' ').map(h=>code[h]))
+  var games = input.split('\n').map(x => x.split(' '))
   var score = (p1,p2) => {
     if (p1 == 'p') {
       if (p2 == 'p') return 3 + 2
@@ -25,6 +25,33 @@ main = input => {
       if (p2 == 'r') return 6 + 1
     }
   }
-  var part1 = games.reduce((a,b)=>score(b[0],b[1])+a,0)
-  return [part1,0]
+  var part1 = games.map(g => g.map(h=>code[h])).reduce((a,b)=>score(b[0],b[1])+a,0)
+
+  var code2 = {
+    A: 'r',
+    X: 0,
+    B: 'p',
+    Y: 3,
+    C: 's',
+    Z: 6
+  }
+  var score2 = (p1,p2) => {
+    if (p1 == 'p') {
+      if (p2 == 0) return p2 + 1
+      if (p2 == 3) return p2 + 2
+      if (p2 == 6) return p2 + 3
+    }
+    if (p1 == 'r') {
+      if (p2 == 0) return p2 + 3
+      if (p2 == 3) return p2 + 1
+      if (p2 == 6) return p2 + 2
+    }
+    if (p1 == 's') {
+      if (p2 == 0) return p2 + 2
+      if (p2 == 3) return p2 + 3
+      if (p2 == 6) return p2 + 1
+    }
+  }
+  var part2 = games.map(g => g.map(h=>code2[h])).reduce((a,b)=>score2(b[0],b[1])+a,0)
+  return [part1,part2]
 }

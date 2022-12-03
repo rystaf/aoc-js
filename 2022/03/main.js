f56a271705409d128488c4780c4253903e62066e
Ryan Stafford <ryan@nanorex.net>
2022-12-03 09:57:22 -0500

    part 2

--- 2022/03/main.js
+++ 2022/03/main.js
@@ -1,13 +1,28 @@
 main = input => { 
   var rucksacks = input.split('\n')
+
+  var part1 = rucksacks
     .map(x => [
       x.slice(0,x.length/2).split(''),
       x.slice(x.length/2).split('')
     ])
-  var part1 = rucksacks.map(r => r[0].find(item => r[1].includes(item)))
+    .map(r => r[0].find(item => r[1].includes(item)))
     .map(t => t.charCodeAt())
     .map(p => p - (p > 96 ? 96 : 64-26))
     .reduce((a,b)=>a+b,0)
 
-  return [part1,0]
+  var part2 = rucksacks
+    .map(x => x.split(''))
+    .reduce((a,b,i) => {
+    n = Math.trunc(i/3)
+    if (!a[n]) a[n] = []
+    a[n].push(b)
+    return a
+  },[])
+    .map(r => r[0].find(item => r[1].includes(item) && r[2].includes(item)))
+    .map(t => t.charCodeAt())
+    .map(p => p - (p > 96 ? 96 : 64-26))
+    .reduce((a,b)=>a+b,0)
+
+  return [part1,part2]
 }

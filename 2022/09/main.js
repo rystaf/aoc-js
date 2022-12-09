6b7d9fcac5e22132cd772afc7bec988968f7fb0f
Ryan Stafford <ryan@nanorex.net>
2022-12-09 11:44:41 -0500

    part 2

--- 2022/09/main.js
+++ 2022/09/main.js
@@ -1,49 +1,51 @@
 main = input => {
-  g = false
   const moves = input.split('\n').map(x => x.split(' ')).map(x => [x[0], parseInt(x[1])])
-  const h = [0,0]
-  const t = [0,0]
-  let i = 0
-  let s = 0
-  const visits = {}
-  if (g) console.log(h,t)
-  while(i<moves.length){
-    let [d,n] = moves[i]
-    if (d == "U") {
-      h[1] += 1
+  const move = knots => {
+    const rope = [...Array(knots).keys()].map(x => [0,0])
+    let i = 0
+    let s = 0
+    const visits = {}
+    while(i<moves.length){
+      let [d,n] = moves[i]
+      if (d == "U") {
+        rope[0][1] += 1
+      }
+      if (d == "D") {
+        rope[0][1] -= 1
+      }
+      if (d == "R") {
+        rope[0][0] += 1
+      }
+      if (d == "L") {
+        rope[0][0] -= 1
+      }
+      rope.forEach((k, i) => {
+        if (!i) return;
+        let x = rope[i-1][0]-rope[i][0]
+        let y = rope[i-1][1]-rope[i][1]
+        if (Math.abs(y) > 1) {
+          rope[i][1] += y > 0 ? 1 : -1
+          if (Math.abs(x) <= 1) rope[i][0] = rope[i-1][0]
+        }
+        if (Math.abs(x) > 1) {
+          rope[i][0] += x > 0 ? 1 : -1
+          if (Math.abs(y) <= 1) rope[i][1] = rope[i-1][1]
+        }
+      })
+      let t = rope[rope.length-1] 
+      if (!visits[t[0]]) visits[t[0]] = {}
+      visits[t[0]][t[1]] = true
+      if (s == n-1) {
+        i += 1
+        s = 0
+      } else {
+        s += 1
+      }
     }
-    if (d == "D") {
-      h[1] -= 1
-    }
-    if (d == "R") {
-      h[0] += 1
-    }
-    if (d == "L") {
-      h[0] -= 1
-    }
-    let x = h[0]-t[0]
-    let y = h[1]-t[1]
-    if (Math.abs(x) > 1) {
-      t[0] += x > 0 ? 1 : -1
-      t[1] = h[1]
-    }
-    if (Math.abs(y) > 1) {
-      t[1] += y > 0 ? 1 : -1
-      t[0] = h[0]
-    }
-    if (g) console.log(d,n,h,t)
-    if (!visits[t[0]]) visits[t[0]] = {}
-    visits[t[0]][t[1]] = true
-    if (s == n-1) {
-      i += 1
-      s = 0
-    } else {
-      s += 1
-    }
+    return Object.keys(visits)
+      .reduce((a,x)=>a+Object.keys(visits[x]).length, 0)
   }
-  if (g) console.log(visits)
-  var part1=Object.keys(visits)
-    .reduce((a,x)=>a+Object.keys(visits[x]).length, 0)
-  var part2=0
+  var part1 = move(2)
+  var part2 = move(10)
   return [part1, part2]
 }

d9d88d4b43342870f9d731e6d542d53dd783f9e9
Ryan Stafford <ryan@nanorex.net>
2022-12-11 12:41:12 -0500

    clean

--- 2022/11/main.js
+++ 2022/11/main.js
@@ -5,22 +5,20 @@ main = input => {
       items: l[1].slice("Starting items:   ".length).split(', ').map(x => parseInt(x)),
       op: (old) => eval(l[2].slice("  Operation: new = ".length)),
       d: parseInt(l[3].slice("  Test: divisible by ".length)),
-      test: (wl) => wl % parseInt(l[3].slice("  Test: divisible by ".length)) == 0
-        ? parseInt(l[4].slice("    If true: throw to monkey ".length))
-        : parseInt(l[5].slice("    If false: throw to monkey ".length))
+      t: parseInt(l[4].slice("    If true: throw to monkey ".length)),
+      f: parseInt(l[5].slice("    If false: throw to monkey ".length))
     }))
-  var common = monkeys.reduce((a,b)=>a*b.d,1)
+  var common = monkeys.reduce((a,{d})=>a*d,1)
   var business = (rounds, relief) => {
     let inspections = monkeys.map(x => 0)
     for (let r = 0; r < rounds; r+=1) {
       monkeys.forEach((m,i)=>{
-        while(item = monkeys[i].items.shift()){
+        while(item = m.items.shift()){
           inspections[i] += 1
-          item = monkeys[i].op(item)
-          if (item >= common) item = item % common
+          item = m.op(item)
+          if (item >= common) item %= common
           if (relief) item = Math.floor(item/3)
-          let test = monkeys[i].test(item)
-          monkeys[monkeys[i].test(item)].items.push(item)
+          monkeys[item % m.d ? m.f : m.t].items.push(item)
         }
       })
     }

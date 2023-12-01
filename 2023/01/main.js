6ee9c2d6a00a902b90f12bd7eaca22fa7b3956e9
Ryan Stafford <ryan@nanorex.net>
2023-12-01 13:29:09 -0500

    cleanup

--- 2023/01/main.js
+++ 2023/01/main.js
@@ -1,12 +1,4 @@
 main = input => {
-  const part1 = input.split('\n').reduce((a,b) => {
-    const digits = b.split('').filter(x => !isNaN(x))
-    if (!digits.length) {
-      return a
-    }
-    return a + parseInt(digits[0] + digits[digits.length - 1])
-  }, 0)
-
   const numbers = [
     'one',
     'two',
@@ -19,21 +11,21 @@ main = input => {
     'nine',
   ]
 
-  const part2 = input.split('\n').reduce((a,b) => {
-    let digits = []
-    b.split('').forEach((l,i)=>{
+  return input.split('\n').reduce((total, line) => {
+    const digits = [[], []] // part1, part2
+    line.split('').forEach((l, i)=>{
       if (!isNaN(l)) {
-        digits.push(l)
+        digits.map((x, i) => digits[i].push(l))
         return
       }
-      numbers.forEach((w,n) => {
-        if (b.substr(0,i+1).slice(w.length * -1) == w) {
-          digits.push((n+1).toString())
+      numbers.forEach((word, n) => {
+        if (line.slice(i, i + word.length) == word) {
+          digits[1].push((n + 1).toString())
         }
       })
     })
-    return a + parseInt(digits[0] + digits[digits.length - 1])
-  }, 0)
-
-  return [part1, part2]
+    return total.map((t, i)=> digits[i].length
+      ? t + parseInt(digits[i][0] + digits[i][digits[i].length - 1])
+      : t)
+  }, [0, 0])
 }

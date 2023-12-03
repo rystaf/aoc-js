13ff632402ad22b651ac562d80738c773f01e47b
Ryan Stafford <ryan@nanorex.net>
2023-12-03 14:02:31 -0500

    part 2

--- 2023/03/main.js
+++ 2023/03/main.js
@@ -1,25 +1,43 @@
 main = input => {
+  const findSymbol = (x,y,s) => {
+    for(let row = -1; row < 2; row++) {
+      for(let col = -1; col < 2; col++) {
+        let sym;
+        if (!s[y+row] || !(sym = s[y+row].split('')[x+col])) continue;
+        if (sym != "." && isNaN(sym)) return x+col+(y+row)*s[0].length;
+      }
+    }
+    return false
+  }
 
-  const adjacent = (list, x) => list.filter((l, i) => (i >= x-1) && (i <= x+1))
+  const gears = input.split('').filter(x => x !="\n")
+    .map((x,i)=> x=="*" ? i : 0)
+    .filter(x => x)
+    .reduce((a,b)=>({...a, [b]: []}),{})
 
-  return input.split('\n').reduce((total, line, y, s) => {
+  const part1 = input.split('\n').reduce((total, line, y, s) => {
     let n = '';
-    let valid = false;
+    let symbol = false;
     line.split('').forEach((l, x) => {
       if (!isNaN(l)) {
         n += l
-        valid = valid || adjacent(s, y)
-          .flatMap(line => adjacent(line.split(''), x))
-          .some(x => x != "." && isNaN(x))
+        symbol = symbol || findSymbol(x,y,s)
       }
       if (isNaN(l) || x == line.length - 1) {
-        if (n.length && valid) {
-          total[0] += parseInt(n)
+        if (n.length && symbol) {
+          total += parseInt(n)
+          if (gears[symbol]) gears[symbol].push(parseInt(n))
         }
         n = '';
-        valid = false;
+        symbol = false;
       }
     })
     return total
-  }, [0, 0])
+  }, 0)
+
+  const part2 = Object.keys(gears).reduce((total, g) => total + ((gears[g].length == 2)
+    ? gears[g].reduce((a,b) => a*b, 1)
+    : 0), 0)
+
+  return [part1, part2]
 }

08d3c08bee570ba222810ca784904fe44ed518c7
Ryan Stafford <ryan@nanorex.net>
2023-12-04 06:33:40 -0500

    part 2

--- 2023/04/main.js
+++ 2023/04/main.js
@@ -1,9 +1,19 @@
 main = input => {
-  return input.split('\n').reduce((total, line) => {
-    const [have, winning] = line.slice(line.indexOf(':')+2).split('|')
-      .map(x => x.split(' ').filter(x => x.length).map(x => parseInt(x)))
-    total[0] += have.filter(h => winning.includes(h)).reduce((t, n) => !t ? 1 : t * 2, 0)
-
+  const copies = input.split('\n').map(x => 1)
+  return input.split('\n').reduce((total, line, i) => {
+    for (true; copies[i] > 0; copies[i]--) {
+      total[1] += 1
+      const [have, winning] = line.slice(line.indexOf(':')+2).split('|')
+        .map(x => x.split(' ').filter(x => x.length).map(x => parseInt(x)))
+      const winners = have.filter(h => winning.includes(h))
+      copies.forEach((c, ci) => {
+        if (ci <= i || ci > (i+winners.length)) return
+        copies[ci]++
+      })
+      if (copies[i] == 1) {
+        total[0] += winners.reduce((t, n) => !t ? 1 : t * 2, 0);
+      }
+    }
     return total
   }, [0, 0])
 }

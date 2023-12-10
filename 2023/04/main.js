bef59547d962602538153b25d99dddf48dc18116
Ryan Stafford <ryan@nanorex.net>
2023-12-10 12:37:26 -0500

    simplified

--- 2023/04/main.js
+++ 2023/04/main.js
@@ -1,19 +1,15 @@
 main = input => {
   const copies = input.split('\n').map(x => 1)
   return input.split('\n').reduce((total, line, i) => {
-    for (true; copies[i] > 0; copies[i]--) {
-      total[1] += 1
-      const [have, winning] = line.slice(line.indexOf(':')+2).split('|')
-        .map(x => x.split(' ').filter(x => x.length).map(x => parseInt(x)))
-      const winners = have.filter(h => winning.includes(h))
-      copies.forEach((c, ci) => {
-        if (ci <= i || ci > (i+winners.length)) return
-        copies[ci]++
-      })
-      if (copies[i] == 1) {
-        total[0] += winners.reduce((t, n) => !t ? 1 : t * 2, 0);
-      }
-    }
+    const [have, winning] = line.slice(line.indexOf(':')+2).split('|')
+      .map(x => x.split(' ').filter(x => x.length).map(x => parseInt(x)))
+    const winners = have.filter(h => winning.includes(h))
+    copies.forEach((c, ci) => {
+      if (ci <= i || ci > (i+winners.length)) return
+      copies[ci] += copies[i]
+    })
+    total[0] += winners.reduce((t, n) => !t ? 1 : t * 2, 0);
+    total[1] += copies[i]
     return total
   }, [0, 0])
 }
